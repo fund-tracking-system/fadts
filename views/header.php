b6a42e10ee8d821bbed8e02f5561e0b5a1c3c2de
yasiruPadmasiri <66236929+yasiruPadmasiri@users.noreply.github.com>
2020-08-19 10:52:03 +0530

    begin

---
+++ views/header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/main.css">
+    <title>Document</title>
+</head>
+
+<body>
+<nav class="navbar navbar-expand-lg ">
+	
+    <h1 style=<center>
+    <a class="navbar-brand"" href="#" >codeigniter CRUD Operations With Bootstrsps</a>
+    </h1>
+    <div>
+    
+    </div>
+</nav>
+    
+    
