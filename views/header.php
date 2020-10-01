5431b68c6c6d9eaa7f6e5530855ee740169b6c04
thisaraMJ <madushantjg@gmail.com>
2020-10-01 12:31:09 +0530

    Convert all to the new version

--- views/header.php
+++ views/header.php
@@ -3,11 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/main.css">
     <title>Document</title>
 </head>
+
 <body>
-    <h1>fund Tracking System</h1>
-    <h2>thisara Madushan</h2>
-    <h2>thisara Madushan</h2>
-</body>
-</html>
+    
+    
