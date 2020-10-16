a650b6403ce164c64cb8ab27a605e300b44a09d6
yasiruPadmasiri <66236929+yasiruPadmasiri@users.noreply.github.com>
2020-10-16 17:38:09 +0530

    vilage officer update people css fixed set style one css file

--- app/view/ministry/fundDeatailsView.php
+++ app/view/ministry/fundDeatailsView.php
@@ -2,16 +2,19 @@
 
 <?php include VIEW.'ministry/sidebar.php' ?>
 
-<div class="SearchByCriteriaform1">
 
-   <form>
-      
-         <fieldset
-            style="background-color:#fffef4; ">
-            <h2 style="margin-bottom:50px;">View Fund Details</h2>
-            <fieldset Style="margin:2%;border-radius:5px; padding:2%; ">
-               <legend style="padding-left:5px; padding-right:5px;"><b>&nbsp;Search Pre Defined Functions</b></legend>
-               <div class="form-row" style="margin-top:5%;">
+<div class="all_bacground_clor">
+
+    <div class="SearchByCriteriaform1">
+
+        <form method="post" action="" id="form">
+
+            <fieldset style="background-color:#fffef4; border-bottom:10px; padding-bottom:12px;">
+                <h2 style="margin-bottom:50px;">VIEW FUND DETAILS</h2>
+                <fieldset Style="margin:2%;border-radius:5px; padding:2%; ">
+                    <legend style="padding-left:5px; padding-right:5px;"><b>&nbsp;Search Pre Defined Functions</b>
+                    </legend>
+                    <div class="form-row" style="margin-top:5%;">
                         <label for="select-fund" style="margin-left:200px;margin-bottom:18px;"><b>Select Predefined
                                 Fund:</b></label>
                         <select name="select-fund" id="select-fund" name="fund" class="form-control"
@@ -27,66 +30,67 @@
                     <button style="margin-left: 78%; margin-top:-50px; width:13%; margin-bottom:20px;" type="submit"
                         class="btn btn-primary">Select</button>
 
-            
-            
+
+
+                    </fieldset> 
+                </fieldset>
+        </form>
+
+
+
+        <form>
+            <fieldset style="background-color:#fffef4;">
+
+                <fieldset Style="margin:2%;border-radius:5px; padding-top:2%;">
+                    <legend style="padding-left:5px; padding-right:5px;"><b>&nbsp;Search Manually Defined Funds</b>
+                    </legend>
+
+                    <div class="form-row" style="margin-top:50px;">
+                        <label for="FundName" style="margin-left:200px;margin-bottom:23px;"><b>Fund
+                                Name:</b></label>
+                        <input class="form-control" id="fundName" name="fundName"
+                            style="margin-left:350px;margin-bottom:50px;margin-right:250px; margin-top:-4%;"
+                            aria-describedby="validationServer03Feedback" required></input>
+
+                    </div>
+                    <div class="form-row " style="margin-bottom:30px;">
+                        <label for="fundType" style="margin-left:200px;margin-bottom:18px;"><b>Fund
+                                Type:</b></label>
+                        <select class="form-control" name="Fund" id="Fund" name="fund"
+                            style="margin-left:350px;margin-bottom:20px;margin-right:250px; margin-top:-3.5%" ;>
+                            <option value="Fire">Fire </option>
+                            <option value="Flood">Flood </option>
+                            <option value="lewgini">Tsunami </option>
+                            <option value="Nayayeem">Nayayeem </option>
+                            <option value="Sulisulan">Sulisulan </option>
+                            <option value="Lewgini">Lewgini </option>
+                        </select>
+                    </div>
+                    <div class="form-row">
+                        <label for="date" style="margin-left:200px;margin-bottom:18px;"><b>Disaster
+                                Date:</b></label>
+                        <input placeholder="YYYY/MM/DD" class="form-control" id='date' name="date"
+                            style="margin-left:350px;margin-bottom:60px;margin-right:250px; margin-top:-3.5%;"
+                            aria-describedby="validationServer03Feedback" required></input>
+                        <small></small>
+                    </div>
+
+                    <div class='button '>
+
+                        <button type="submit" class='submit-button  btn btn-primary'
+                            style=" float: right; padding-left:30px;padding-right:30px;padding-top:10px; padding-bottom:10px; margin-right:240px; margin-Bottom:30px;;">Search
+                        </button>
+                    </div>
+
+
+
+                </fieldset>
             </fieldset>
-         </fieldset>
-   </form>
-
-
-
-   <form>
-      <fieldset
-         style="background-color:#fffef4;">
-
-         <fieldset Style="margin:2%;border-radius:5px; padding-top:2%;">
-            <legend style="padding-left:5px; padding-right:5px;"><b>&nbsp;Search Manually Defined Funds</b></legend>
-
-            <div class="form-row" style="margin-top:50px;">
-               <label for="FundName" style="margin-left:200px;margin-bottom:23px;"><b>Fund Name:</b></label>
-               <input class="form-control" id="fundName" name="fundName"
-                  style="margin-left:350px;margin-bottom:50px;margin-right:250px; margin-top:-4%;"
-                  aria-describedby="validationServer03Feedback" required></input>
-
-            </div>
-            <div class="form-row " style="margin-bottom:30px;">
-               <label for="fundType" style="margin-left:200px;margin-bottom:18px;"><b>Fund Type:</b></label>
-               <select class="form-control" name="Fund" id="Fund" name="fund"
-                  style="margin-left:350px;margin-bottom:20px;margin-right:250px; margin-top:-3.5%" ;>
-                  <option value="Fire">Fire </option>
-                  <option value="Flood">Flood </option>
-                  <option value="lewgini">Tsunami </option>
-                  <option value="Nayayeem">Nayayeem </option>
-                  <option value="Sulisulan">Sulisulan </option>
-                  <option value="Lewgini">Lewgini </option>
-               </select>
-            </div>
-            <div class="form-row">
-               <label for="date" style="margin-left:200px;margin-bottom:18px;"><b>Disaster Date:</b></label>
-               <input placeholder="YYYY/MM/DD" class="form-control" id='date' name="date"
-                  style="margin-left:350px;margin-bottom:60px;margin-right:250px; margin-top:-3.5%;"
-                  aria-describedby="validationServer03Feedback" required></input>
-               <small></small>
-            </div>
-
-            <div class='button '>
-
-               <button type="submit" class='submit-button  btn btn-primary'
-                  style=" float: right; padding-left:30px;padding-right:30px;padding-top:10px; padding-bottom:10px; margin-right:240px; margin-Bottom:30px;;">Search
-               </button>
-            </div>
-
-
-
-         </fieldset>
-      </fieldset>
 
 
 
 
-   </form>
-
-
-
+        </form>
+    </div>
 </div>
 <?php include VIEW.'footer.php'?>
--- app/view/villageOfficer/UpdatePeopleView.php
+++ app/view/villageOfficer/UpdatePeopleView.php
@@ -2,72 +2,58 @@
 <?php include VIEW.'villageOfficer/sidebar.php' ?>
 
 <div class="all_bacground_clor">
-
     <div class="SearchByCriteriaform1">
 
+
+    
         <form method="post" action="" id="form">
-
-
-            <fieldset style="background-color:#fffef4; border-bottom:10px; padding-bottom:12px;">
+            <fieldset class="searchBackgroundFS">
                 <h2 style="margin-bottom:50px;">Search By NID</h2>
-                <fieldset
-                    style="padding-top:15px; margin-right:50px; margin-left:20px;border-radius:10px; padding:3%; ">
+                <fieldset class="searchBar">
                     <div class="form-row ">
-
-                        <label for="NID-number" style="margin-left:150px; margin-top:5px;margin-bottom:-25px; "><b>
-                                NID Number:</b></label>
-                        <input class="form-control " id='NID-number'
-                            style="position: relative;margin-left:50px; margin-bottom:10px;width: 400px;height: 30px;border-radius: 5px;outline: none;border-width: 1.5px; padding:20px;"
-                            placeholder="9 7 2 8 1 0 1 7 7 v" name="NID"></input>
-
-                        <button style="margin-left: 70%; margin-top:-50px; width:13%; margin-bottom:20px;" type="submit"
-                            class="btn btn-primary">Search</button>
+                        <label for="NID-number" class="searchBarLable"><b>NID Number:</b></label>
+                        <input class="form-control searchInput" id='NID-number' placeholder="9 7 2 8 1 0 1 7 7 v"
+                            name="NID"></input>
+                        <button type="submit" class="btn btn-primary btnNav">Search</button>
                     </div>
-
                 </fieldset>
-
             </fieldset>
-
         </form>
 
 
-        <form>
-            <fieldset style="background-color:#fffef4; border-bottom:10px; padding-bottom:12px;padding-top:20px; ">
-                <h2 style="margin-bottom:50px;">UPDATE PEOPLE DATA</h2>
 
+
+        <form>
+            <fieldset class=" searchBackgroundFS">
+                <h2 style="">UPDATE PEOPLE DATA</h2>
 
                 <div class="form-row">
-                    <label for="name " style="margin-left:70px;margin-bottom:18px;"><b>Name:</b></label>
-                    <input class="form-control" id="name"
-                        style="margin-left:150px;margin-bottom:20px;margin-right:150px; margin-top:-40px;" name="name"
-                        value="name display only" readonly></input>
+                    <label for="name " class="inputLable"><b>Full Name:</b></label>
+                    <input class="form-control Input" id="name" name="name" value="name display only" readonly></input>
                 </div>
 
                 <div class="form-row">
-                    <label for="Address" style="margin-left:50px;margin-bottom:-35px;"><b> Address:</b></label>
-                    <input class="form-control" id="address"
-                        style="margin-left:150px;margin-bottom:20px;margin-right:150px; margin-top:-8px;" name="address"
+                    <label for="Address" class="inputLable"><b>Address:</b></label>
+                    <input class="form-control Input" id="address" name="address"
                         placeholder="Opatha,ganegoda,Elptye,Galle"></input>
                 </div>
 
-                <div style="display:flex;">
-                    <label for="birth-date" style="margin-left:70px;margin-bottom:18px;"><b>Birth Date:</b></label>
-                    <input class="form-control" id='birthDate' style="margin-left:30px;margin-right:150px;" name="birthDate"
-                        value="Birth date read only" readonly></input>
+                <div>
+                    <label for="birth-date" class="inputLable"><b>Birth Date:</b></label>
+                    <input class="form-control InputOne" id='birthDate' name="birthDate" value="YYYY/MM/DD"
+                        readonly></input>
                     <small> </small>
-                    <label for="birth-date" style="margin-left:70px;margin-bottom:18px;"><b>Birth Certificate
+                    <label for="birthNO" class="inputLable LableTwo"><b>Birth Certificate
                             No:</b></label>
-                    <input class="form-control" id='birthvertifiacateNo'
-                        style="margin-left:150px;margin-bottom:20px;margin-right:150px;" name="birthCertificateNo"
-                        aria-describedby="validationServer03Feedback" readonly></input>
+                    <input class="form-control InputTwo" id='birthvertifiacateNo' name="birthCertificateNo"
+                        readonly></input>
                 </div>
 
 
 
                 <div class="form-row">
-                    <label for="region" style="margin-left:70px;margin-bottom:18px;"><b>Region:</b></label>
-                    <select class="form-control" name="region"
-                        style="margin-left:150px;margin-bottom:20px;margin-right:150px;margin-top:-40px;" id="region" name="region">
+                    <label for="region" class="inputLable"><b>Region:</b></label>
+                    <select class="form-control Input" name="region" id="region" name="region">
                         <option value="volvo">Volvo</option>
                         <option value="saab">Saab</option>
                         <option value="mercedes">Mercedes</option>
@@ -75,58 +61,44 @@
                     </select>
                 </div>
 
-                <div style="display:flex;">
-                    <label class='phone-number-label' for="job"
-                        style="margin-left:70px;margin-bottom:18px;"><b>Job:</b></label>
-                    <input class="form-control" id='job' style="margin-left:40px;margin-bottom:20px;margin-right:150px;" name="job"></input>
-                    <label for='job-type' style="margin-left:70px;margin-bottom:18px;"><b>Job Type:</b></label>
-                    <input class="form-control" id='job-type'
-                        style="margin-left:150px;margin-bottom:20px;margin-right:150px;"
-                        name="JobType"></input>
+
+                <div>
+                    <label class='inputLable' for="job"><b>Job:</b></label>
+                    <input class="form-control InputOne" id='job' name="job"></input>
+                    <label for='job-type' class="inputLable LableTwo"><b>Job Type:</b></label>
+                    <input class="form-control InputTwo" id='job-type' name="JobType"></input>
                 </div>
 
 
 
-                <fieldset
-                    style="margin-top:20px;margin-bottom:20px;margin-left:20px; margin-right:110px;padding-top:30px; border-radius:10px; ">
-                    <div class="form-row">
-                        <label for="trustee" style="margin-left:70px;margin-bottom:-35px;"><b>Trustee
-                            </b></label>
-                        <input class="form-control" id="trustee"
-                            style="margin-left:150px;margin-bottom:20px;margin-right:150px; margin-top:-8px;"
-                            aria-describedby="validationServer03Feedback" readonly></input>
-                    </div>
-
-                    <div class="form-row">
-                        <div style="display:flex;">
-                            <label class='phone-number-label' for="phone-number"
-                                style="margin-left:70px;margin-bottom:18px;"><b>Phone Number1:</b></label>
-                            <input class="form-control" id='phone-number-1'
-                                style="margin-left:10px;margin-bottom:20px;margin-right:150px;"
-                                aria-describedby="validationServer03Feedback" readonly name="phoneNumber1"></input>
-
-
-
-
-                            <label class='phone-number-label' for="phone-number"
-                                style="margin-left:70px;margin-bottom:18px;"><b>Phone Number2:</b></label>
-                            <input class="form-control" id='phone-number2'
-                                style="margin-left:150px;margin-bottom:20px;margin-right:150px;"name="phoneNumber2"
-                                aria-describedby="validationServer03Feedback" readonly></input>
-                        </div>
-                    </div>
-                </fieldset>
                 <div class="form-row">
-                    <label class='trustee-label' for="CivilStatus"
-                        style="margin-left:70px;margin-bottom:18px; margin-top:30px;"><b>Civil</br>status:</b></label>
-                    <select class="form-control" name="trustee" id="civilStatus"
-                        style="margin-left:150px;margin-bottom:20px;margin-right:150px; margin-top:-60px;"name="civilStatus">
+                    <label for="trustee" class="inputLable"><b>Trustee
+                        </b></label>
+                    <input id="trustee" class="form-control Input" readonly></input>
+                </div>
+
+
+
+                <div>
+                    <label class='inputLable' for="phone-number"><b>Phone Number1:</b></label>
+                    <input class="form-control InputOne" id='phone-number-1' readonly name="phoneNumber1"></input>
+
+                    <label class='inputLable LableTwo' for="phone-number"><b>Phone Number2:</b></label>
+                    <input class="form-control InputTwo" id='phone-number2' name="phoneNumber2" readonly></input>
+                </div>
+
+
+
+                <div class="form-row">
+                    <label class="inputLable" for="CivilStatus"><b>Civil</br>status:</b></label>
+                    <select class="form-control Input" name="trustee" id="civilStatus" name="civilStatus">
                         <option value="UnMarrried">Married</option>
                         <option value="Married">Maried</option>
                     </select>
-
                 </div>
-                <div class='buttons' style="margin-left:870px;">
+
+
+                <div class="Twobtn">
 
                     <button type="submit" class='confirm-button btn btn-primary '>Confirm</button>
                     <button class='confirm-button btn btn-primary '>View List</button>
