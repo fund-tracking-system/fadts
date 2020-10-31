6f57a9574a316cf067aaf775a3187c22673d621f
Hextream <49858479+Hextream@users.noreply.github.com>
2020-10-31 14:28:02 +0530

    clean

--- app/view/includes/indexView.php
+++
@@ -1,4 +0,0 @@
-<?php include VIEW.'includes/header.php' ?>
-<?php include VIEW.'includes/sidebar.php' ?>
-
-<?php include VIEW.'includes/footer.php' ?>
--- fadts/app/view/includes/footer.php
+++
@@ -1,5 +0,0 @@
-<footer class="dashboard">
-		<h4>© 2020, 2nd Year Group Project | University of Colombo School of Computing</h4>
-	</footer>
-</body>
-</html>
