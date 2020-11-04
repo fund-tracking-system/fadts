ce5b5c357e2ee4244c4da08a68b1ab356907389f
Jihani-31 <jihaninanayakkara@gmail.com>
2020-11-04 21:46:45 +0530

    add new disaster updated model

--- app/model/divisionalSec/addNewDisasterSaveModel.php
+++ app/model/divisionalSec/addNewDisasterSaveModel.php
@@ -6,19 +6,23 @@
     //create connection
 
 
-
+echo $_SESSION['disaster_type'];
     $query='INSERT INTO disaster (type,name,date,description) VALUES(?,?,?,?)';
     $stmt=$con->prepare($query);
-    $stmt->bind_param('sssss',$disaster,$disasterName,$date,$description);
+    $stmt->bind_param('ssss',$disaster,$disasterName,$date,$description);
 
 
 
 
     $disaster= $_SESSION['disaster_type'];
+   // echo $disaster;   
     $disasterName= $_SESSION['disaster_disasterName'];
+
     $date= $_SESSION['disaster_date'];
     $description= $_SESSION['disaster_description'];
 
+    echo $disaster;
+    echo $disasterName;
 
 
 
@@ -28,7 +32,7 @@
 
 
      //redirecting to view
-     header("Location:/fadts/divisional/ViewAddNewDisaster"); 
+    // header("Location:/fadts/divisional/ViewAddNewDisaster"); 
 
 
      $con->close();
