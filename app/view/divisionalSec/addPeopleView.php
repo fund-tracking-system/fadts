e761a4ae2c634d8fdaed570b0f66bd8c40db63d7
thisaraMJ <madushantjg@gmail.com>
2020-11-08 00:14:50 +0530

    header change

--- app/view/divisionalSec/addPeopleView.php
+++ app/view/divisionalSec/addPeopleView.php
@@ -2,17 +2,17 @@
 <?php include VIEW.'includes/sidebar.php' ?>
 
 <div class="all_bacground_clor">
-   <div class="SearchByCriteriaform1">
+    <div class="SearchByCriteriaform1">
 
 
 
-      <form method="post" id="formAddPeople" action="/fadts/divisional/addPeopleModel">
-         <fieldset class="BackgroundFS">
+        <form method="post" id="formAddPeople" action="/fadts/divisional/addPeopleModel">
+            <fieldset class="BackgroundFS">
             <h2> ADD PEOPLE DATA </h2>
-            <fieldset class="searchBar">
+                <fieldset class="searchBar">
+                    
 
-
-               <?php
+                    <?php
 
 
                 if(isset($_GET['error'])){
@@ -26,78 +26,78 @@
                 ?>
 
 
-               <div class="form-row">
-                  <label for="Name" class="inputLable"><b>Name:</b></label>
-                  <input class="form-control Input" id="name" name="name" placeholder="Name In Full"></input>
+                    <div class="form-row">
+                        <label for="Name" class="inputLable"><b>Name:</b></label>
+                        <input class="form-control Input" id="name" name="name" placeholder="Name In Full"></input>
 
-               </div>
-
-
-
-               <div class="form-row">
-                  <label for="nid" class="inputLable"><b>NID:</b></label>
-                  <input class="form-control Input" id='nid' name="nid"></input>
-               </div>
+                    </div>
 
 
 
-               <div class="form-row">
-                  <label for="nid" class="inputLable"><b>Family Head :</b></label>
-                  <input class="form-control Input" id='headOfFamily' name="headOfFamily"></input>
-               </div>
+                    <div class="form-row">
+                        <label for="nid" class="inputLable"><b>NID:</b></label>
+                        <input class="form-control Input" id='nid' name="nid"></input>
+                    </div>
 
 
 
-               <div class="form-row">
-                  <label for="name " class="inputLable"><b>Adrress:</b></label>
-                  <input class="form-control Input" id="address" name="address"></input>
-               </div>
+                    <div class="form-row">
+                        <label for="nid" class="inputLable"><b>Family Head :</b></label>
+                        <input class="form-control Input" id='headOfFamily' name="headOfFamily"></input>
+                    </div>
 
 
 
-               <div class="form-row">
-                  <label for="birth-date" class="inputLable"><b>Birth Date:</b></label>
-                  <input class="form-control Input" id='b_date' name="b_date" type="date"></input>
-                  <small> </small>
-               </div>
-
-               <div class="form-row">
-                  <label for="birthNO" class="inputLable"><b>Birth Certificate
-                        No:</b></label>
-                  <input class="form-control Input" id='b_certifi' name="b_certifi"></input>
-               </div>
+                    <div class="form-row">
+                        <label for="name " class="inputLable"><b>Adrress:</b></label>
+                        <input class="form-control Input" id="address" name="address"></input>
+                    </div>
 
 
-               <!-- <div class="form-row">
+
+                    <div class="form-row">
+                        <label for="birth-date" class="inputLable"><b>Birth Date:</b></label>
+                        <input class="form-control Input" id='b_date' name="b_date" type="date"></input>
+                        <small> </small>
+                    </div>
+
+                    <div class="form-row">
+                        <label for="birthNO" class="inputLable"><b>Birth Certificate
+                                No:</b></label>
+                        <input class="form-control Input" id='b_certifi' name="b_certifi"></input>
+                    </div>
+
+
+                    <!-- <div class="form-row">
                     <label for="email" class="inputLable"><b>Email
                         </b></label>
                     <input type="email" class="form-control Input" id="email" name="email"
                         placeholder="name@example.com" type="email">
                 </div> -->
 
-               <div class="form-row">
-                  <label for="jobtype" class="inputLable"><b>Job type:</b></label>
-                  <select class="form-control Input" name="jobType" id="jobType">
-                     <option value="Goverment">Goverment</option>
-                     <option value="Private">Private</option>
-                     <option value="Retired">Retired</option>
-                     <option value="SelfEmployee">SelfEmployee</option>
-                     <option value="Jobless">Jobless</option>
+                    <div class="form-row">
+                        <label for="jobtype" class="inputLable"><b>Job type:</b></label>
+                        <select class="form-control Input" name="jobType" id="jobType">
+                            <option value="Goverment">Goverment</option>
+                            <option value="Private">Private</option>
+                            <option value="Retired">Retired</option>
+                            <option value="SelfEmployee">SelfEmployee</option>
+                            <option value="Jobless">Jobless</option>
 
-                  </select>
-               </div>
+                        </select>
+                    </div>
 
 
-               <!-- <div class='form-row'>
+                    <!-- <div class='form-row'>
                     <label class="inputLable"><b>Job type </br></label>
                     <label class="inputLable"></b></label>
 
                     <select class="form-control Input" onclick="showCheckboxes() ">
                         <option>Select an option</option>  select krapua pennanna hadann ooona -->
-               <!-- </select> -->
-               <!-- <div class="overSelect"></div> -->
+                    <!-- </select> -->
+                    <!-- <div class="overSelect"></div> -->
 
-               <!-- <div id="checkboxes" class="checksbox" name="checkbox">
+                    <!-- <div id="checkboxes" class="checksbox" name="checkbox">
 
                         <label for="one" class="form-control">
                             <input type="checkbox" id="Goverment" name="Goverment" />&nbsp; &nbsp;Goverment</label>
@@ -114,70 +114,70 @@
                         <label for="four" class="form-control">
                             <input type="checkbox" id="Jobless" name="jobless" /> &nbsp; &nbsp;Jobless</label>
                     </div> -->
-               <!-- </div> -->
+                    <!-- </div> -->
 
 
 
-               <div class="form-row">
-                  <label class="inputLable" for="CivilStatus"><b>Civil status:</b></label>
-                  <select class="form-control Input" id="civilStatus" name="civilStatus">
-                     <option value="1">Unmarried</option>
-                     <option value="0">Married</option>
-                  </select>
-               </div>
+                    <div class="form-row">
+                        <label class="inputLable" for="CivilStatus"><b>Civil status:</b></label>
+                        <select class="form-control Input" id="civilStatus" name="civilStatus">
+                            <option value="1">Unmarried</option>
+                            <option value="0">Married</option>
+                        </select>
+                    </div>
 
 
 
 
 
-               <div class="form-row">
-                  <label for="tnid" class="inputLable"><b>Trustee:</b></label>
-                  <input class="form-control Input" id='tnid' class="form-control Input" name="trustee"></input>
-               </div>
+                    <div class="form-row">
+                        <label for="tnid" class="inputLable"><b>Trustee:</b></label>
+                        <input class="form-control Input" id='tnid' class="form-control Input" name="trustee"></input>
+                    </div>
 
-               <div class="form-row">
-                  <label for="tnid" class="inputLable"><b>Region:</b></label>
-                  <input class="form-control Input" id='region' class="form-control Input" name="region"></input>
-               </div>
-
-
-
-               <div class="form-row">
-                  <label class="inputLable" for="phone-number"><b>Contact Number 1:</b></label>
-                  <input class="form-control Input" id='phonenumber1' name="phoneNumber1" type="number"></input>
-               </div>
-               <div class="form-row">
-
-                  <label class="inputLable" for="phone-number"><b>Contact Number 2:</b></label>
-                  <input class="form-control Input" id='phonenumber2' name="phoneNumber2" type="number"></input>
-               </div>
+                    <div class="form-row">
+                        <label for="tnid" class="inputLable"><b>Region:</b></label>
+                        <input class="form-control Input" id='region' class="form-control Input" name="region"></input>
+                    </div>
 
 
 
-               <button type="submit" class=' btn btn-primary  signlebtn'>Assign Region</button>
-               <!-- <button class=' btn btn-primary '>View List</button> -->
+                    <div class="form-row">
+                        <label class="inputLable" for="phone-number"><b>Contact Number 1:</b></label>
+                        <input class="form-control Input" id='phonenumber1' name="phoneNumber1" type="number"></input>
+                    </div>
+                    <div class="form-row">
 
+                        <label class="inputLable" for="phone-number"><b>Contact Number 2:</b></label>
+                        <input class="form-control Input" id='phonenumber2' name="phoneNumber2" type="number"></input>
+                    </div>
+
+
+
+                    <button type="submit" class=' btn btn-primary  signlebtn'>Assign Region</button>
+                    <!-- <button class=' btn btn-primary '>View List</button> -->
+
+                </fieldset>
             </fieldset>
-         </fieldset>
 
-      </form>
-
+        </form>
 
 
-   </div>
+
+    </div>
 </div>
 <script type="text/javascript">
 var expanded = false;
 
 function showCheckboxes() {
-   var checkboxes = document.getElementById("checkboxes");
-   if (!expanded) {
-      checkboxes.style.display = "block";
-      expanded = true;
-   } else {
-      checkboxes.style.display = "none";
-      expanded = false;
-   }
+    var checkboxes = document.getElementById("checkboxes");
+    if (!expanded) {
+        checkboxes.style.display = "block";
+        expanded = true;
+    } else {
+        checkboxes.style.display = "none";
+        expanded = false;
+    }
 }
 </script>
 
