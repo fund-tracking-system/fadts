55c4cac0bde2e158a6632a2b21445ff2346d9864
Jihani-31 <jihaninanayakkara@gmail.com>
2020-11-13 21:19:47 +0530

    new bar chart with animation

--- app/view/includes/indexView.php
+++ app/view/includes/indexView.php
@@ -53,30 +53,19 @@ switch($controller){
   $(function () {
       // ChartJS
       var ctx = document.getElementById('Chart3').getContext('2d');
+      console.log(Chart.defaults.scale.ticks);
+      Chart.defaults.scale.ticks.beginAtZero=true;
       var chart = new Chart(ctx, {
-         type: 'line', // The type of chart we want to create
+         type: 'bar', // The type of chart we want to create
          data: {
-            labels: ['2015', '2016', '2017', '2018', '2019', '2020'],
+            labels: ['January','February','March','April','May','June','July','August','September','Octomber','November','December'],
             datasets: [{
-                  label: '# fund release',
-                  data: [12, 19, 3, 5, 2, 3],
-                  backgroundColor: [
-                     'rgba(255, 99, 132, 0.2)',
-                     'rgba(54, 162, 235, 0.2)',
-                     'rgba(255, 206, 86, 0.2)',
-                     'rgba(75, 192, 192, 0.2)',
-                     'rgba(153, 102, 255, 0.2)',
-                     'rgba(255, 159, 64, 0.2)'
-                  ],
-                  borderColor: [
-                     'rgba(255, 99, 132, 1)',
-                     'rgba(54, 162, 235, 1)',
-                     'rgba(255, 206, 86, 1)',
-                     'rgba(75, 192, 192, 1)',
-                     'rgba(153, 102, 255, 1)',
-                     'rgba(255, 159, 64, 1)'
-                  ],
-                  borderWidth: 1
+                  label: 'Fund Release Per Month',
+                  data: [100,200,400,300,250,140,370,200,300,200],
+                  backgroundColor:'#4cd84c',
+                  hoverBackgroundColor:'#00FF00',
+                  // borderColor:'#00FF00',
+                  borderWidth: 2
             }]
          },
          options: {
