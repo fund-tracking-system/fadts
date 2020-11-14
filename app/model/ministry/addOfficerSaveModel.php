e6d0924b15a957dcc9ee5bb91a4c98b651260154
Hextream <49858479+Hextream@users.noreply.github.com>
2020-11-15 02:43:48 +0530

    add officer and assign region design refinements

--- app/model/ministry/addOfficerSaveModel.php
+++ app/model/ministry/addOfficerSaveModel.php
@@ -18,40 +18,42 @@
     */
 
     //database connection file calling
-    require 'connectionOOP.php';
+    require 'connectionOOP.php';	
     
     //prepare and bind
-    $query = 'INSERT INTO officer (nid, password, email, region, loginStatus, name, position) VALUES (?, ?, ?, ?, ?, ?, ?)';
+    $query = 'INSERT INTO user (username, usernid, email, password, userrole, region, loginStatus) VALUES (?, ?, ?, ?, ?, ?, ?)';
     $stmt = $con->prepare($query);
-    $stmt->bind_param("sssssss", $nid, $password, $email, $region, $loginStatus, $name, $position);
+    $stmt->bind_param("sssssss", $name, $nid, $email, $password, $position, $region, $loginStatus);
  
     //assign values from post
-    $nid = $_SESSION['officer_nid']; 
-    $password = $_SESSION['officer_password'];
+    $name = $_SESSION['officer_name'];
+    $nid = $_SESSION['officer_nid'];
     $email = $_SESSION['officer_email'];
+    //$position = $_SESSION['assign_region_level'];   
     $region = $_SESSION['assign_region_id']; 
     $loginStatus = $_SESSION['officer_loginStatus'];
-    $name = $_SESSION['officer_name'];
-    //$position = $_SESSION['assign_region_level']; 
-
+    
     //determine officer position from region level
     switch ($_SESSION['assign_region_level']) {
         case 0:
             $position = 'auditor';
             break;
         case 1:
-            $position = 'provincial secretary';
+            $position = 'provincialsec';
             break;
         case 2:
-            $position = 'district secretary';
+            $position = 'districtsec';
             break; 
         case 3:
-            $position = 'divisional secretary';
+            $position = 'divisionalsec';
             break;
         case 4:
-            $position = 'village officer';
+            $position = 'villageofficer';
             break;    
     }
+
+    //default password is set initially
+    $password = "123456";
 
     //execute prepared statement
     $stmt->execute();
@@ -61,13 +63,13 @@
     $con->close();
 
     //unset form data stored in session variables
+    unset($_SESSION['officer_name']);
     unset($_SESSION['officer_nid']); 
-    unset($_SESSION['officer_password']); 
-    unset($_SESSION['officer_email']); 
-    unset($_SESSION['assign_region_id']); 
-    unset($_SESSION['officer_loginStatus']); 
-    unset($_SESSION['officer_name']); 
+    unset($_SESSION['officer_email']);
+    //unset($_SESSION['officer_password']);  
     unset($_SESSION['assign_region_level']);
+    unset($_SESSION['assign_region_id']); 
+    unset($_SESSION['officer_loginStatus']);  
     
     //redirecting to view
     header("Location:/fadts/ministry/addOfficerView"); 
--- app/view/ministry/addOfficerView.php
+++ app/view/ministry/addOfficerView.php
@@ -5,28 +5,25 @@
     <div class="SearchByCriteriaform1">
         <form method="post" action="/fadts/ministry/addOfficerModel">
             <fieldset class="BackgroundFS">
-                <!--
-                <h2 style="color:black ; margin-bottom:55px;padding-top:10px; ">Add Officer Form</h2>
-                -->
                 <h2> ADD OFFICER FORM </h2>
 
-                <fieldset class="searchBar">                
+                <fieldset class="searchBar">   
+                    <div class="form-row">
+                        <label for="name" class="inputLable"><b>Name</b></label>
+                        <input class="form-control Input" name="name"></input>
+                    </div>
+
                     <div class="form-row">
                         <label for="nid" class="inputLable"><b>NID:</b></label>
                         <input class="form-control Input" name="nid"></input>
                     </div>
 
-                    <div class="form-row" style="margin-bottom:20px;">
-                        <label for="name" class="inputLable"><b>Name</b></label>
-                        <input class="form-control Input" name="name"></input>
-                    </div>
-
-                    <div class="form-row" style="margin-bottom:20px;">
+                    <div class="form-row">
                         <label for="email" class="inputLable"><b>E-mail:</b></label>
                         <input class="form-control Input" type="email" name='email'></input>
                     </div> 
 
-                    <div class="form-row" style="margin-bottom:20px;">
+                    <div class="form-row">
                         <label for="loginStatus" class="inputLable"><b>Login status:</b></label>
                         <select class="form-control Input" name="loginStatus">
                             <option value="permitted">Permitted</option>
@@ -34,7 +31,7 @@
                         </select>
                     </div>
 
-                    <div class='buttons' style="margin-left:900px;">
+                    <div class='buttons'>
                         <button type="submit" name= "submit" class='btn btn-primary signlebtn'>Next</button> 
                     </div>
                 </fieldset>    
