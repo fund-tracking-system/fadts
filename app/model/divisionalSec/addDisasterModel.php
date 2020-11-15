3d584f11f69c10a861593b7837035e6d202621aa
thisaraMJ <madushantjg@gmail.com>
2020-11-15 13:35:20 +0530

    add new disaster completed

--- app/model/divisionalSec/addDisasterModel.php
+++ app/model/divisionalSec/addDisasterModel.php
@@ -7,20 +7,45 @@ if(isset($_POST['submit'])){
 
   $userId = $_SESSION['userid'];
   
-  $type = $_POST['type'];
   $name = $_POST['name'];
+  $type = $_POST['type'];
   $date = $_POST['date'];
   $region = $_POST['region'];
   $description = $_POST['description'];
 
+ 
   $sql = "INSERT INTO disaster (name,type,date,description,creator) VALUES (?,?,?,?,?)";
   $stmt = mysqli_stmt_init($con);
 
   if(!mysqli_stmt_prepare($stmt,$sql)){
     mysqli_close($con);
-    header("Location:/fadts/home/index?error=db_conn_err");
+    header("Location:/fadts/divisional/addDisaster?error=db_conn_err");
     exit();
   }else{
+    mysqli_stmt_bind_param($stmt,"sssss",$name,$type,$date,$description,$userId);
+
+    if(mysqli_stmt_execute($stmt)){
+      $disasterId = mysqli_insert_id($con);
+
+      foreach($region as $regionId) { 
+        $sql = "INSERT INTO disasterregion (regionId,disasterId) VALUES ($regionId,$disasterId)";
+
+        if(!mysqli_query($con,$sql)){
+          mysqli_close($con);
+          header("Location:/fadts/divisional/addDisaster?error=db_conn_err");
+          exit();
+        }
+      }
+      mysqli_close($con);
+      header("Location:/fadts/divisional/addDisaster?error=success");
+      exit();
+
+    }else{
+      mysqli_close($con);
+      header("Location:/fadts/divisional/addDisaster?error=db_conn_err");
+      exit();
+    }
+   
 
   }
 
--- app/view/divisionalSec/addPeopleView.php
+++ app/view/divisionalSec/addPeopleView.php
@@ -23,7 +23,6 @@
                 ?>
 
 
-<<<<<<< HEAD
                     <div class="form-row">
                         <label for="Name" class="inputLable"><b>Name :</b></label>
                         <input class="form-control Input" id="name" name="name" placeholder="Name in full"></input>
@@ -37,22 +36,10 @@
                         <input class="form-control Input" id='nid' name="nid"></input>
                     </div>
 
-
-                    <!-- 
-                    <div class="form-row">
-                        <label for="nid" class="inputLable"><b>Family Head :</b></label>
-                        <input class="form-control Input" id='headOfFamily' name="headOfFamily"></input>
-                    </div> -->
-
-
-
-
                     <div class="form-row">
                         <label for="name " class="inputLable"><b>Adrress :</b></label>
                         <input class="form-control Input" id="address" name="address"></input>
                     </div>
-
-
 
                     <div class="form-row">
                         <label for="birth-date" class="inputLable"><b>Birth Date :</b></label>
@@ -67,7 +54,7 @@
                     </div>
 
                     <div class="form-row">
-                        <label for="jobtype" class="inputLable"><b>Job Type :</b></label>
+                        <label for="jobtype" class="inputLable"><b>Income Type :</b></label>
                         <select class="form-control Input" name="jobType" id="jobType">
                             <option value="" disabled selected>Select job type</option>
                             <option value="Goverment">Goverment</option>
@@ -113,95 +100,14 @@
                   $_SESSION['region_result']=$res
                   
                   ?>
-
-
                         <select id='region' class='form-control Input' name='region' id='region'
                             style='position:sticky;top:60px;overflow:scroll;  width:550px; '>
                             <option value="" disabled selected>Select region</option>
-                            <?php 
-                             foreach($res as $data){
-=======
-               <div class="form-row">
-                  <label for="Name" class="inputLable"><b>Name :</b></label>
-                  <input class="form-control Input" id="name" name="name" placeholder="Name in full"></input>
-
-               </div>
-
-               <div class="form-row">
-                  <label for="nid" class="inputLable"><b>NIC :</b></label>
-                  <input class="form-control Input" id='nid' name="nid"></input>
-               </div>
-
-               <div class="form-row">
-                  <label for="name " class="inputLable"><b>Adrress :</b></label>
-                  <input class="form-control Input" id="address" name="address"></input>
-               </div>
-
-
-
-               <div class="form-row">
-                  <label for="birth-date" class="inputLable"><b>Birth Date :</b></label>
-                  <input class="form-control Input" id='b_date' name="b_date" type="date"></input>
-                  <small> </small>
-               </div>
-
-               <div class="form-row">
-                  <label for="birthNO" class="inputLable"><b>Birth Certificate
-                        No :</b></label>
-                  <input class="form-control Input" id='b_certifi' name="b_certifi"></input>
-               </div>
-
-               <div class="form-row">
-                  <label for="jobtype" class="inputLable"><b>Income Type :</b></label>
-                  <select class="form-control Input" name="jobType" id="jobType">
-                     <option value="" disabled selected>Select job type</option>
-                     <option value="Goverment">Goverment</option>
-                     <option value="Private">Private</option>
-                     <option value="Retired">Retired</option>
-                     <option value="SelfEmployee">SelfEmployee</option>
-                     <option value="Jobless">Jobless</option>
-
-                  </select>
-               </div>
-
-               <div class="form-row">
-                  <label class="inputLable" for="CivilStatus"><b>Civil Status :</b></label>
-                  <select class="form-control Input" id="civilStatus" name="civilStatus">
-                     <option value="" disabled selected>Select civil status</option>
-                     <option value="1">Unmarried</option>
-                     <option value="0">Married</option>
-                  </select>
-               </div>
-
-               <div class="form-row">
-                  <label for="tnid" class="inputLable"><b>Trustee :</b></label>
-                  <input class="form-control Input" id='tnid' class="form-control Input" name="trustee"></input>
-               </div>
-
-               <div class="form-row" style="margin-bottom:50px;">
-
-                  <label for="tnid" class="inputLable" style="margin-right:185px;"><b>Region :</b></label>
-                  <?php
-
-                    require 'connection.php'; 
-               
-                    $rgn="SELECT level, regionid, superRegion,name  FROM region  WHERE level=4 ";
-                    $rgnRes=$con->query($rgn) ;
-                    $res=$rgnRes->fetch_all(MYSQLI_ASSOC);
-
-                    $_SESSION['region_result']=$res;  //for assign region  ?>
-
-                  <select id='region' class='form-control Input' name='region' id='region'
-                     style='position:sticky;top:60px;overflow:scroll;  width:530px;'>
-                     <?php 
-                        foreach($res as $data){
-                            if($data['superRegion']==$_SESSION['region']){
->>>>>>> 88d00b72bcc98be927edbeabac6fb1f7c8704b3c
+                           <?php 
+                              foreach($res as $data){
                                 echo '<option value="'.$data['regionid'].'">'.$data['name'].'</option>';
-
-                            }
-                        }
-                    ?>
+                              }
+                           ?>
                </div>
                <div><input type="hidden"></input></div>
 
