cf8e7a01b5402c6a60a35fa64bee0775d9f136bf
Jihani-31 <jihaninanayakkara@gmail.com>
2020-11-16 14:17:03 +0530

    auditor dashboard

--- app/view/includes/indexView.php
+++ app/view/includes/indexView.php
@@ -461,14 +461,17 @@ window.onload = calendar;
    
 ?>
 
+
 <div class="divisionDashboard">
    <div class="grid_box">
       <div class="box-1">
+      <div class="nav_link"><B>CHANGES MADE</B></div>
+
          <canvas id="Chart1"></canvas>
       </div>
       <div class="box-2">
-         
-        
+      <!-- <div class="nav_link"><B>FUND DELIVERY ANALYSIS</B></div> -->
+         <canvas id="Chart2"></canvas>
       </div>
       <div class="box-3" id="box_3">
       <!-- <h1><B><?php echo $_SESSION['username']?></B></h1>
@@ -480,7 +483,7 @@ window.onload = calendar;
             <p id="calendar_month"></p>
             <p id="calendar_year"></p>
          </div>
-         <div id="calendar_events">
+         <div id="calendar_events" style="height:100%;">
          <h3>Your Events</h3>
             <!-- <?php echo $_SESSION['userrole']; ?> -->
             <?php
@@ -510,11 +513,165 @@ window.onload = calendar;
       </div>
       <div class="box-4">
          <div>
-            <canvas id="Chart"></canvas>
+            <canvas id="Chart3"></canvas>
          </div>
       </div>
    </div>
 </div>
+
+<script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
+<!-- ChartJS -->
+<script src="https://cdn.jsdelivr.net/npm/chart.js@2.8.0"></script>
+<script>
+   $( document ).ready(function() {
+      $('#box_3').click(function(){
+         window.location.href='/fadts/calender/ViewCalender';
+      })
+   });
+
+
+   $(function () {
+      var ctx = document.getElementById('Chart1').getContext('2d');
+      var chart = new Chart(ctx, {
+         type: 'doughnut',
+         data: {
+            labels: ['Village Officer', 'Division Secretary','Ministry',],
+            datasets: [{
+                  label: '# fund release',
+                  data: [20,5,25],
+                  backgroundColor: [
+                     '#16a085',
+                     '#f1c40f',
+                     '#2980b9'
+                  ],
+                  borderColor: [
+                     '#16a085',
+                     '#f1c40f',
+                     '#2980b9'
+                  ],
+                  borderWidth: 1
+            }]
+         },
+         options: {
+            rotation:Math.PI*-10.5,
+            animation:{
+               animatescale:true
+            },
+            
+         }
+      }); 
+      chart.canvas.parentNode.style.height = '100%';
+
+  });
+
+  $(function () {
+      // ChartJS
+      var ctx = document.getElementById('Chart2').getContext('2d');
+      console.log(Chart.defaults.scale.ticks);
+      Chart.defaults.scale.ticks.beginAtZero=true;
+      var chart = new Chart(ctx, {
+         type: 'horizontalBar', // The type of chart we want to create
+         data: {
+            labels: ['January','February','March','April','May','June','July','August','September','Octomber','November','December'],
+            datasets: [{
+                  label: 'Victims per Year',
+                  data: [100,200,400,300,250,140,370,200,300,200],
+                  // backgroundColor:'#4cd84c',
+                  hoverBackgroundColor:'#00FF00',
+                  borderColor:'#00FF00',
+                  borderWidth: 2
+            }]
+         },
+         options: {
+            scales: {
+                  yAxes: [{
+                     ticks: {
+                        beginAtZero: true
+                     }
+                  }]
+            },
+            maintainAspectRatio: false
+
+         }
+      }); 
+      chart.canvas.parentNode.style.height = '100%';
+
+  });
+
+
+
+  
+  $(function () {
+      // ChartJS
+      var ctx = document.getElementById('Chart3').getContext('2d');
+      console.log(Chart.defaults.scale.ticks);
+      Chart.defaults.scale.ticks.beginAtZero=true;
+      var chart = new Chart(ctx, {
+         type: 'line', // The type of chart we want to create
+         data: {
+            labels: ['January','February','March','April','May','June','July','August','September','Octomber','November','December'],
+            datasets: [{
+                  label: 'deliveries Made',
+                  data: [100,200,400,300,250,140,370,200,300,200],
+                  backgroundColor:'#4cd84c',
+                  hoverBackgroundColor:'#00FF00',
+                  borderColor:'#00FF00',
+                  borderWidth: 2
+            }]
+         },
+         options: {
+            scales: {
+                  yAxes: [{
+                     ticks: {
+                        beginAtZero: true
+                     }
+                  }]
+            },
+            maintainAspectRatio: false
+
+         }
+      }); 
+      chart.canvas.parentNode.style.height = '100%';
+
+  });
+
+
+  
+
+
+function calendar(){
+   var day=['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+   var month=['January','February','March','April','May','June','July','August','September','October','November','December'];
+   var d=new Date();
+   console.log(day[d.getDay()]);
+   $('#calendar_day').text(day[d.getDay()]);
+   // setText('calendar_day', day[d.getDay()]);
+   setText('calendar_date', d.getDate());
+   setText('calendar_month', month[d.getMonth()]);
+   setText('calendar_year', (1900+d.getYear()));
+};
+
+//this function will set the text value of tags
+function setText(id, val){
+    if(val < 10){
+        val = '0' + val;    //add leading 0 if val < 10
+    }
+    document.getElementById(id).innerHTML = val;
+};
+
+//call calendar() when page load
+window.onload = calendar;
+
+
+</script>
+
+
+
+
+
+
+
+
 
 
 
