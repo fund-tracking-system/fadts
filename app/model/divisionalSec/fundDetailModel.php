4c4685a6c1dba37324562e8bff120286ad3a0a8a
yasiruPadmasiri <66236929+yasiruPadmasiri@users.noreply.github.com>
2020-12-26 15:13:23 +0530

    fund details view

--- app/model/divisionalSec/fundDetailModel.php
+++ app/model/divisionalSec/fundDetailModel.php
@@ -1,77 +1,75 @@
-<?php
+                    <?php
 
-session_start();
+                    session_start();
 
-if(isset($_GET['fundId'])){
+                    if(isset($_GET['fundId'])){
 
-   require 'connection.php';   // database connection file calling
+                    require 'connection.php';   // database connection file calling
 
-   $fundId = $_GET['fundId'];
-   $dilivered=0;
-   $unDelivered=0;
-   $totlRecipients=0;
-   $officer_region=$_SESSION['region'];
+                    $fundId = $_GET['fundId'];
+                    $dilivered=0;
+                    $unDelivered=0;
+                    $totlRecipients=0;
+                    $officer_region=$_SESSION['region'];
 
-   $sql="SELECT recipient.personId,recipient.deliveryStatus,person.region, region.superRegion  FROM recipient INNER JOIN person ON recipient.personId=person.personId INNER JOIN region ON region.regionId=person.region WHERE recipient.fundId= $fundId AND region.superRegion=$officer_region";
-   $result=$con->query($sql);
-   $res=$result->fetch_all(MYSQLI_ASSOC); 
- 
+                    $sql="SELECT recipient.personId,recipient.deliveryStatus,person.region, region.superRegion  FROM recipient INNER JOIN person ON recipient.personId=person.personId INNER JOIN region ON region.regionId=person.region WHERE recipient.fundId= $fundId AND region.superRegion=$officer_region";
+                    $result=$con->query($sql);
+                    $res=$result->fetch_all(MYSQLI_ASSOC); 
+                    
+                    $sqlName="SELECT name  FROM fund WHERE fund.fundId=$fundId";
+                    $resultName=$con->query($sqlName);
+                    $resName=$resultName->fetch_all(MYSQLI_ASSOC); 
+                    
 
-   var_dump($res);
-foreach($res as $data){
-
-
-    if($data['deliveryStatus']==1){
-        $dilivered++;
-    }
-    else if($data['deliveryStatus']==0){
-        $unDelivered++;
-    } 
+                  
+                    foreach($resName as $datas){
+                       $_SESSION['SelectFundName']= $datas['name'];
 
 
-    echo "<br>";
-    echo $data['region'];
-    echo "<br>";
-    
-}
-echo $dilivered;
-$_SESSION['fundDelivered']=$dilivered;
-echo "<br>";
-echo $unDelivered;
-$_SESSION['fundUndelivered']=$unDelivered;
-$totlRecipients=$dilivered+$unDelivered;
-echo "<br>";
-echo $totlRecipients;
-$_SESSION['totalRecipients']=$totlRecipients;
+                    }
+echo  $_SESSION['SelectFundName'];
+
+                    var_dump($resName);
+                    foreach($res as $data){
 
 
-$Fdate="SELECT publishedTime ,amountPerPerson FROM fund WHERE fundId=$fundId";
-$dateRes=$con->query($Fdate);
-$resDate=$dateRes->fetch_all(MYSQLI_ASSOC); 
-var_dump($resDate);
-foreach($resDate as $day){
-        $_SESSION['publishedTime']= $day['publishedTime'];
-        $_SESSION['amountPerPerson']=$day['amountPerPerson'];
+                        if($data['deliveryStatus']==1){
+                            $dilivered++;
+                        }
+                        else if($data['deliveryStatus']==0){
+                            $unDelivered++;
+                        } 
 
 
-}
+                        echo "<br>";
+                        echo $data['region'];
+                        echo "<br>";
+                        
+                    }
+                    echo $dilivered;
+                    $_SESSION['fundDelivered']=$dilivered;
+                    echo "<br>";
+                    echo $unDelivered;
+                    $_SESSION['fundUndelivered']=$unDelivered;
+                    $totlRecipients=$dilivered+$unDelivered;
+                    echo "<br>";
+                    echo $totlRecipients;
+                    $_SESSION['totalRecipients']=$totlRecipients;
 
 
-header("Location:/fadts/divisional/selectFundDetailsView"); 
+                    $Fdate="SELECT publishedTime ,amountPerPerson FROM fund WHERE fundId=$fundId";
+                    $dateRes=$con->query($Fdate);
+                    $resDate=$dateRes->fetch_all(MYSQLI_ASSOC); 
+                    var_dump($resDate);
+                    foreach($resDate as $day){
+                            $_SESSION['publishedTime']= $day['publishedTime'];
+                            $_SESSION['amountPerPerson']=$day['amountPerPerson'];
 
 
+                    }
 
 
-
-
-
-
-
-
-
-
-
-
+                  header("Location:/fadts/divisional/selectFundDetailsView"); 
 
 
 
@@ -85,5 +83,19 @@ header("Location:/fadts/divisional/selectFundDetailsView");
 
 
 
-}
-?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                    }
+                    ?>
--- app/model/divisionalSec/loadFundDetailsModel.php
+++ app/model/divisionalSec/loadFundDetailsModel.php
@@ -5,11 +5,11 @@ session_start();
 require 'connection.php'; 
 
 
-$sql="SELECT fundId	, name,region,publishedTime FROM fund";
-$result=$con->query($sql);
-$res=$result->fetch_all(MYSQLI_ASSOC); 
-$_SESSION['fundList']=$res;
-var_dump($res);
+        $sql="SELECT fundId	, name,region,publishedTime FROM fund";
+        $result=$con->query($sql);
+        $res=$result->fetch_all(MYSQLI_ASSOC); 
+        $_SESSION['fundList']=$res;
+        var_dump($res);
 
 
 
