14b5f2bf85e4a9bf9b25cc055df34ece8fc21db7
yasiruPadmasiri <66236929+yasiruPadmasiri@users.noreply.github.com>
2021-02-11 20:53:33 +0530

    disaster victims list

--- app/view/divisionalSec/disasterVictimsView.php
+++ app/view/divisionalSec/disasterVictimsView.php
@@ -10,61 +10,8 @@ require 'connection.php';
 
 <div class="all_bacground_clor">
 	<div class="SearchByCriteriaform1">
-	<h2 style="margin-bottom:30px;"> DISASTER DETAILS ANALISYS</h2>
-		<div class="grid_box1">
-			<div class="box1">
-				<div>
-				<h1>Disaster Distribution</h1>
-				</div>
-
-				<canvas id="Chart1" style="padding:5px; "></canvas>
-				<!-- <canvas id="Chart1" style="height: 370px; width: 70%;"></canvas> -->
-				
-			</div>
-			<div class="box9">
-				<div style="display:inline-block;">
-					<a href="/fadts/divisional/disasterVictimsView" class="btn btn-primary" style="float:left; margin: 15px; margin-top: 55px;"><B>Victim List</B></a>
-
-				</div>
-			</div>
-			<!-- <div style="display:inline-block;">
-				<a href="" class="btn btn-primary" style="float:left;"><B>Victim Search</B></a>
-				<a href="" class="btn btn-primary" style="float:right;"><B>Add Victims</B></a>
-						
-				
-				
-				
-			</div>
-		 -->
-			<div class="box2">
-				<h1 class="dashboard-title">Disaster Name</h1>
-				<h3 class="fontwhite" > <?php echo  $_SESSION['disasterName'];?></h3>
-			</div>
-			<div class="box3">
-				<h1 class="dashboard-title">Disaster Type</h1>
-				<h3 class="fontwhite" ><?php echo  $_SESSION['disasterType'];?></h3>
-			</div>
-			<div class="box4">
-				<h1 class="dashboard-title">Disaster Date</h1>
-				<h3 class="fontwhite" ><?php echo  $_SESSION['disasterDate'];?></h3>
-			</div>
-			<div class="box5">
-				<h1 class="dashboard-title">Disaster creater</h1>
-				<h3 class="fontwhite" ><?php echo $_SESSION['userrole']?></h3>
-			</div>
-			<div class="box6">
-				<h1 class="dashboard-title">Victims Amount</h1>
-				<h3 class="fontwhite" ><?php echo $_SESSION['victimsCount']?></h3>
-			</div>
-			<div class="box7">
-				<h1 class="dashboard-title">Total Damage</h1>
-				<h3 class="fontwhite" ><?php echo $_SESSION['totalDamege']?></h3>
-			</div>
-			<div class="box8">
-				<!-- <h1>Affected Area</h1> -->
-				<h1 class="dashboard-title"><?php echo  $_SESSION['regionName']?> Division Secertory </h1>
-			</div>
-		</div>
+	<h2 style="margin-bottom:30px;"> DISASTER VICTIMS LIST</h2>
+	
 
 
 		<fieldset class="tableBar">
