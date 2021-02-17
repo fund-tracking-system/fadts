a2baa644f88bc801ac33686da630d1011b89ff38
yasiruPadmasiri <66236929+yasiruPadmasiri@users.noreply.github.com>
2021-02-17 16:11:51 +0530

    ministry load  fund details table

--- app/model/ministry/loadFundDetails.php
+++ app/model/ministry/loadFundDetails.php
@@ -10,13 +10,13 @@ echo $myRegion;
 
 
   
-  $sql4="SELECT fund.fundId,fund.name,region.level as lvl,region.name as ren ,fundregion.regionId ,fund.publishedTime FROM  fund  right join fundregion On fund.fundId=fundregion.fundId inner join  region ON region.regionId =fundregion.regionId WhERE region.regionid=$myRegion or fundregion.regionId=$myRegion or fundregion.regionId=1 or fundregion.regionId= $provincialRegion or fundregion.regionId=$districtRegion or fundregion.regionId=$divisionalRegion";        
+  $sql4="SELECT fund.fundId,fund.name,region.level as lvl,region.name as ren ,fundregion.regionId ,fund.publishedTime FROM  fund  right join fundregion On fund.fundId=fundregion.fundId inner join  region ON region.regionId =fundregion.regionId";        
   $result4=$con->query($sql4);
   $res4=$result4->fetch_all(MYSQLI_ASSOC);
   $_SESSION['fundList']=$res4;
 
   
-// header("Location:/fadts/ministry/fundDetails"); 
+header("Location:/fadts/ministry/fundDetails"); 
 
 
 
