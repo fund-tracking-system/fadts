6ed67dc6dd4052b3a5db82e37dc4c610e86fe325
yasiruPadmasiri <66236929+yasiruPadmasiri@users.noreply.github.com>
2021-02-17 15:51:40 +0530

    district region remove

--- app/model/ministry/loadFundDetails.php
+++ app/model/ministry/loadFundDetails.php
@@ -9,20 +9,6 @@ echo $myRegion;
   
 
 
-
-
-  
-  $sql3="SELECT superRegion  From region Where region.regionId= $districtRegion";
-  $result3=$con->query($sql3);
-  $res3=$result3->fetch_all(MYSQLI_ASSOC);
-  foreach($res3 as $data3){ 
-  $_SESSION['provincialRegion']=$data2['superRegion'];
-
-  }
-
-
-  $provincialRegion= $_SESSION['provincialRegion'];      //save provincial region
-
   
   $sql4="SELECT fund.fundId,fund.name,region.level as lvl,region.name as ren ,fundregion.regionId ,fund.publishedTime FROM  fund  right join fundregion On fund.fundId=fundregion.fundId inner join  region ON region.regionId =fundregion.regionId WhERE region.regionid=$myRegion or fundregion.regionId=$myRegion or fundregion.regionId=1 or fundregion.regionId= $provincialRegion or fundregion.regionId=$districtRegion or fundregion.regionId=$divisionalRegion";        
   $result4=$con->query($sql4);
