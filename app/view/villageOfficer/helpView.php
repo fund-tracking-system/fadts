d959aebd7f6f7c3068ad3f971bf55f825b21743d
Thishanjude <thishan1997@gmail.com>
2021-03-22 12:32:35 +0530

    create helpview4

--- app/view/villageOfficer/helpView.php
+++ app/view/villageOfficer/helpView.php
@@ -11,7 +11,7 @@
     <ul>
     <li class="help">Initially the User has to enter his correct NIC number in the Search bar and click the Search button or Press Enter.</li><br>
     <li>Then the User data with Full name and Birth Date will be displayed only and other fields are kept empty for Updating.<br> The User can't
-    change his Full Name and Birth Date</li><br>
+    change his Full Name and Birth Date.</li><br>
     <li>Now the User can change the details Address, Family Head, Monthly Income, Prolonged Disease/Disorder,<br> Civil Status, Income type
      and Dead/Alive.</li><br>
     <li>After entering everything click on Confirm and Update the entered new data will be Updated.</li>
@@ -19,11 +19,11 @@
 
     <h3>SEARCH PEOPLE DATA</h3>
     <ul>
-    <li>Here User can search the data using NIC or Search by Criteria</li><br>
+    <li>Here User can search the data using NIC or Search by Criteria.</li><br>
     <li>Initially User can Search the details by entering correct NIC number. If the correct NIC is entered and<br> Search button is clicked
-    the data will be displayed</li><br>
+    the data will be displayed.</li><br>
     <li>Otherwise the user can Search the data by criteria. User can filter the data by entering age limit,<br> Prolonged Disease/Disorder,
-    Civil Status, Gross Income, Current Funds, Income type. After entering filter data <br>click Search Here button the data will be displayed</li>
+    Civil Status, Gross Income, Current Funds, Income type. After entering filter data <br>click Search Here button the data will be displayed.</li>
     </ul><br><br>
 
     <h3>VIEW DISASTER DETAILS</h3>
@@ -32,7 +32,7 @@
     data by entering terms in the Search bar.</li><br>
     <li>When the User clicks the View button the detailed analysis like Disaster Name, Disaster Type, Disaster Date,<br> Disaster Creater, 
     Number of victims, Total damage and GN division of the particular disaster will be displayed.</li><br>
-    <li>When the User clicks the Victim List button the Detailed information of Victims of the particular disaster will be displayed</li>
+    <li>When the User clicks the Victim List button the Detailed information of Victims of the particular disaster will be displayed.</li>
     </ul><br><br>
 
     <h3>VIEW FUND DETAILS</h3>
@@ -41,10 +41,23 @@
     data by entering terms in the Search bar.</li><br>
     <li>When the User clicks the View button the detailed analysis like Fund amount, Fund per person, Fund published date,<br> Analyse region, 
     Number of delivered person and Undelivered person of the particular fund will be displayed.</li><br>
-    <li>When the User clicks the Recipient List button the Detailed information of Recipients of the particular Fund will be displayed</li>
+    <li>When the User clicks the Recipient List button the Detailed information of Recipients of the particular Fund will be displayed.</li>
     </ul><br><br>
 
-    </ul>
+    <h3>FUND RELEASE</h3>
+    <ul>
+    <li>User enter this page and type correct NIC number in the box and click Search button.</li><br>
+    <li>Then the User can view the fund details that is released corresponding to that particular NIC.</li><br>
+    </ul><br><br>
+
+    <h3>ADD DISASTER VICTIMS</h3>
+    <ul>
+    <li>User enter Add disaster victims page and can view the list of Disasters that occured and the details related to that disasters<br>
+    i.e. Disaster type, Disaster name, Disaster occured region and Date.</li><br>
+    <li>When the User wants to add a victim of by a particular Disaster, The user can click on the Add Victim button corrosponding to<br>
+     the particular Disaster type and can add the Victim  by entering Victim's NIC number.</li><br>
+    </ul><br><br>
+
 
      
     
