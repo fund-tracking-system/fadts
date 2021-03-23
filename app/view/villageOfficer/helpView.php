09a1c5da48c891db16ba7e73e969298d19ca892f
Thishanjude <thishan1997@gmail.com>
2021-03-23 13:53:08 +0530

    CSS helpview1

--- app/view/villageOfficer/helpView.php
+++ app/view/villageOfficer/helpView.php
@@ -7,54 +7,54 @@
 
     <div class="SearchByCriteriaform1">
 
-    <h3 class="Vh1">UPDATE PEOPLE DATA</h3>
-    <ul>
-    <li class="help">Initially the User has to enter correct NIC number in the Search bar and click the Search button or Press Enter.</li><br>
-    <li>Then the User data with Full name and Birth Date will be displayed only and other fields are kept empty for Updating.<br> The User can't
+    <h3 class ="VOh1">UPDATE PEOPLE DATA</h3>
+    <ul class="VOp1">
+    <li>Initially the User has to enter correct NIC number in the Search bar and click the Search button or Press Enter.</li><br>
+    <li>Then the User data with Full name and Birth Date will be displayed only and other fields are kept empty for Updating.The User can't
     change his Full Name and Birth Date.</li><br>
-    <li>Now the User can change the details Address, Family Head, Monthly Income, Prolonged Disease/Disorder,<br> Civil Status, Income type
+    <li>Now the User can change the details Address, Family Head, Monthly Income, Prolonged Disease/Disorder, Civil Status, Income type
      and Dead/Alive.</li><br>
     <li>After entering everything click on Confirm and Update the entered new data will be Updated.</li>
     </ul><br><br>
 
-    <h3>SEARCH PEOPLE DATA</h3>
-    <ul>
+    <h3 class ="VOh2">SEARCH PEOPLE DATA</h3>
+    <ul class ="VOp2">
     <li>Here User can search the data using NIC or Search by Criteria.</li><br>
-    <li>Initially User can Search the details by entering correct NIC number. If the correct NIC is entered and<br> Search button is clicked
+    <li>Initially User can Search the details by entering correct NIC number. If the correct NIC is entered and Search button is clicked
     the data will be displayed.</li><br>
-    <li>Otherwise the user can Search the data by criteria. User can filter the data by entering age limit,<br> Prolonged Disease/Disorder,
-    Civil Status, Gross Income, Current Funds, Income type. After entering filter data <br>click Search Here button the data will be displayed.</li>
+    <li>Otherwise the user can Search the data by criteria. User can filter the data by entering age limit, Prolonged Disease/Disorder,
+    Civil Status, Gross Income, Current Funds, Income type. After entering filter data click Search Here button the data will be displayed.</li>
     </ul><br><br>
 
-    <h3>VIEW DISASTER DETAILS</h3>
-    <ul>
-    <li>User clicks on View disaster details and enter into the page. Then the user can view the all Disaster details.<br> User can filter the
+    <h3 class="VOh3">VIEW DISASTER DETAILS</h3>
+    <ul class="VOp3">
+    <li>User clicks on View disaster details and enter into the page. Then the user can view the all Disaster details. User can filter the
     data by entering terms in the Search bar.</li><br>
-    <li>When the User clicks the View button the detailed analysis like Disaster Name, Disaster Type, Disaster Date,<br> Disaster Creater, 
+    <li>When the User clicks the View button the detailed analysis like Disaster Name, Disaster Type, Disaster Date, Disaster Creater, 
     Number of victims, Total damage and GN division of the particular disaster will be displayed.</li><br>
     <li>When the User clicks the Victim List button the Detailed information of Victims of the particular disaster will be displayed.</li>
     </ul><br><br>
 
-    <h3>VIEW FUND DETAILS</h3>
-    <ul>
-    <li>When the User clicks on View Fund details and enter into the page. Then the User can view all the Fund details.<br> User can filter the
+    <h3 class="VOh4">VIEW FUND DETAILS</h3>
+    <ul class="VOp4">
+    <li>When the User clicks on View Fund details and enter into the page. Then the User can view all the Fund details. User can filter the
     data by entering terms in the Search bar.</li><br>
-    <li>When the User clicks the View button the detailed analysis like Fund amount, Fund per person, Fund published date,<br> Analyse region, 
+    <li>When the User clicks the View button the detailed analysis like Fund amount, Fund per person, Fund published date, Analyse region, 
     Number of delivered person and Undelivered person of the particular fund will be displayed.</li><br>
     <li>When the User clicks the Recipient List button the Detailed information of Recipients of the particular Fund will be displayed.</li>
     </ul><br><br>
 
-    <h3>FUND RELEASE</h3>
-    <ul>
+    <h3 class="VOh5">FUND RELEASE</h3>
+    <ul class="VOp5">
     <li>User enter this page and type correct NIC number in the box and click Search button.</li><br>
     <li>Then the User can view the fund details if that person corresponding to that NIC is in Recipient list.</li><br>
     </ul><br><br>
 
-    <h3>ADD DISASTER VICTIMS</h3>
-    <ul>
-    <li>User enter Add disaster victims page and can view the list of Disasters that occured and the details related to that disasters<br>
+    <h3 class="VOh6">ADD DISASTER VICTIMS</h3>
+    <ul class="VOp6">
+    <li>User enter Add disaster victims page and can view the list of Disasters that occured and the details related to that disasters
     i.e. Disaster type, Disaster name, Disaster occured region and Date.</li><br>
-    <li>When the User wants to add a victim of by a particular Disaster, The user can click on the Add Victim button corrosponding to<br>
+    <li>When the User wants to add a victim of by a particular Disaster, The user can click on the Add Victim button corrosponding to
      the particular Disaster type and can add the Victim  by entering Victim's NIC number.</li><br>
     </ul><br><br>
 
