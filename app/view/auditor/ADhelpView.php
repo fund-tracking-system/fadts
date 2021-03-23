abd740407921da5afa40822a0f7587e21cf20057
Thishanjude <thishan1997@gmail.com>
2021-03-23 17:13:48 +0530

    CSS ADhelpview1

--- app/view/auditor/ADhelpView.php
+++ app/view/auditor/ADhelpView.php
@@ -7,45 +7,45 @@
 
     <div class="SearchByCriteriaform1">
 
-    <h3>VIEW OFFICER HISTORY</h3>
-    <ul>
+    <h3 class="ADh1">VIEW OFFICER HISTORY</h3>
+    <ul class="ADp1">
     <li>User can enter into the View Officer History page and Enter the NID of the Officer and click on Search button.</li><br>
     <li>After clicking Search button the User can view the Details of the Officer corresponding to that NID.</li>
     </ul><br><br>
 
-    <h3>VIEW ELIGIBILITY HISTORY</h3>
-    <ul>
+    <h3 class="ADh2">VIEW ELIGIBILITY HISTORY</h3>
+    <ul class="ADp2">
     <li>User can enter into the View Eligibility History page and Enter the NID of the Person and click on Search button.</li><br>
     <li>After clicking Search button the User can view the Details of the Eligible list of that corresponding NID.</li>
     </ul><br><br>
 
-    <h3>VIEW PERSON HISTORY</h3>
-    <ul>
+    <h3 class="ADh3">VIEW PERSON HISTORY</h3>
+    <ul class="ADp3">
     <li>User can enter into the View Eligibility History page and Enter the NID of the Person and click on Search button.</li><br>
     <li>After clicking Search button the User can view the past History of that corresponding NID.</li>
     </ul><br><br>
     
-    <h3>VIEW DISASTER DETAILS</h3>
-    <ul>
+    <h3 class="ADh4">VIEW DISASTER DETAILS</h3>
+    <ul class="ADp4">
     <li>User clicks on View disaster details and enter into the page. Then the user has to fill the fields with correct data.</li><br>
     <li>When the User clicks the View Selected Disaster button the detailed analysis of that Disaster will be displayed to the 
     User.</li>
     </ul><br><br>
 
-    <h3>VIEW FUND DETAILS</h3>
-    <ul>
-    <li>When the User clicks on View Fund details and enter into the page. Then the User can view Pre defined funds<br> and can search
+    <h3 class="ADh5">VIEW FUND DETAILS</h3>
+    <ul class="ADp5">
+    <li>When the User clicks on View Fund details and enter into the page. Then the User can view Pre defined funds and can search
     manually defined funds .</li><br>
-    <li>User can select the predefined fund and Search for the details or he can manually fill the data in manually <br>defined funds
+    <li>User can select the predefined fund and Search for the details or he can manually fill the data in manually defined funds
     form and click on Search Here button to get the details of the fund.</li>
     </ul><br><br>
 
-    <h3>SEARCH PEOPLE DATA</h3>
-    <ul>
+    <h3 class="ADh6">SEARCH PEOPLE DATA</h3>
+    <ul class="ADp6">
     <li>Here User can search the data using NIC or Search by Criteria.</li><br>
-    <li>Initially User can Search the details by entering correct NIC number. If the correct NIC is entered and<br> Search button is clicked
+    <li>Initially User can Search the details by entering correct NIC number. If the correct NIC is entered and Search button is clicked
     the data will be displayed.</li><br>
-    <li>Otherwise the user can Search the data by criteria. User can filter the data by entering the data that has to be filtered<br> 
+    <li>Otherwise the user can Search the data by criteria. User can filter the data by entering the data that has to be filtered
     in the fields in the form. After entering the data click Search Here button, then the people data will be displayed.</li>
     </ul><br><br>
 
