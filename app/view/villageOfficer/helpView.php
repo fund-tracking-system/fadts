e37e148e95eef1af84bf3ef92bf8a73a5ba792bc
Thishanjude <thishan1997@gmail.com>
2021-03-24 15:52:44 +0530

    VOhelpview_images2

--- app/view/villageOfficer/helpView.php
+++ app/view/villageOfficer/helpView.php
@@ -33,31 +33,42 @@
     <img src="/fadts/app/images/picvo2_3.jpg" alt="vopic2_3" style="width:800px;height:180px;"><br><br>
     </ul><br><br>
 
-    <h3 class="VOh3">VIEW DISASTER DETAILS</h3>
+    <h3 class="VOh3">VIEW DISASTER DETAILS???</h3>
     <ul class="VOp3">
     <li>User clicks on View disaster details and enter into the page. Then the user can view the all Disaster details. User can filter the
     data by entering terms in the Search bar.</li><br>
+    <img src="/fadts/app/images/picvo3_1.jpg" alt="vopic3_1" style="width:800px;height:380px;"><br><br>
     <li>When the User clicks the View button the detailed analysis like Disaster Name, Disaster Type, Disaster Date, Disaster Creater, 
     Number of victims, Total damage and GN division of the particular disaster will be displayed.</li><br>
-    <li>When the User clicks the Victim List button the Detailed information of Victims of the particular disaster will be displayed.</li>
+    <img src="/fadts/app/images/picvo3_2.jpg" alt="vopic3_2" style="width:800px;height:380px;"><br><br>
+    <li>When the User clicks the Victim List button the Detailed information of Victims of the particular disaster will be displayed.
+    Search bar can be used to filter the results according to the terms.</li><br>
+    <img src="/fadts/app/images/picvo3_3.jpg" alt="vopic3_3" style="width:800px;height:310px;"><br><br>
     </ul><br><br>
 
-    <h3 class="VOh4">VIEW FUND DETAILS</h3>
+    <h3 class="VOh4">VIEW FUND DETAILS???</h3>
     <ul class="VOp4">
     <li>When the User clicks on View Fund details and enter into the page. Then the User can view all the Fund details. User can filter the
     data by entering terms in the Search bar.</li><br>
+    <img src="/fadts/app/images/picvo4_1.jpg" alt="vopic4_1" style="width:800px;height:350px;"><br><br>
     <li>When the User clicks the View button the detailed analysis like Fund amount, Fund per person, Fund published date, Analyse region, 
     Number of delivered person and Undelivered person of the particular fund will be displayed.</li><br>
-    <li>When the User clicks the Recipient List button the Detailed information of Recipients of the particular Fund will be displayed.</li>
+    <img src="/fadts/app/images/picvo4_2.jpg" alt="vopic4_2" style="width:800px;height:380px;"><br><br>
+    <li>When the User clicks the Recipient List button the Detailed information of Recipients of the particular Fund will be displayed.
+     The Recipient list can be filtered using terms in search bar given.</li><br>
+     <img src="/fadts/app/images/picvo4_3.jpg" alt="vopic4_3" style="width:800px;height:320px;"><br><br>
     </ul><br><br>
 
-    <h3 class="VOh5">FUND RELEASE</h3>
+    <h3 class="VOh5">FUND RELEASE???</h3>
     <ul class="VOp5">
     <li>User enter this page and type correct NIC number in the box and click Search button.</li><br>
+    <img src="/fadts/app/images/picvo5_1.jpg" alt="vopic5_1" style="width:800px;height:200px;"><br><br>
+    <li>If the User enters the NIC number of person not in his region the error message will be displayed.</li><br>
+    <img src="/fadts/app/images/picvo5_2.jpg" alt="vopic5_2" style="width:800px;height:200px;"><br><br>
     <li>Then the User can view the fund details if that person corresponding to that NIC is in Recipient list.</li><br>
     </ul><br><br>
 
-    <h3 class="VOh6">ADD DISASTER VICTIMS</h3>
+    <h3 class="VOh6">ADD DISASTER VICTIMS???</h3>
     <ul class="VOp6">
     <li>User enter Add disaster victims page and can view the list of Disasters that occured and the details related to that disasters
     i.e. Disaster type, Disaster name, Disaster occured region and Date.</li><br>
