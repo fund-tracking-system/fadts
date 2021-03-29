5e149c799a11c3900b195624016638207f831cf9
thisaraMJ <madushantjg@gmail.com>
2021-03-29 09:38:00 +0530

    new update 29/03/2021 9.37pm

--- app/model/villageOfficer/fundReleaseModel.php
+++ app/model/villageOfficer/fundReleaseModel.php
@@ -1,47 +1,109 @@
 <?php
 
-if(isset($_POST['otpRequest']) && isset($_GET['entryId'])){
+session_start();
 
+if(isset($_POST['otpRequest']) && isset($_GET['entryId']) && isset($_POST['phone'])){
+   
+   $nic=$_GET['nic'];
    $entryId = $_GET['entryId'];
+
+   $contact = $_POST['phone'];
+   $contact = "94".substr($contact, -9);
+
    $otp = rand(100000,999999);
+   // $otpHashed = password_hash($otp,PASSWORD_DEFAULT);
+
    date_default_timezone_set('Asia/Colombo');
    $sendedTime = date('d-m-Y H:i:s');
+
    $text = "Your requested OTP is $otp on $sendedTime. Please hand this OTP code to your village officer amount in order to collect the due fund amount.";
 
 
-   require_once('../notify/autoload.php');
+   require_once('notify/autoload.php');
 
    $api_instance = new NotifyLk\Api\SmsApi();
    $user_id = "13127"; 
    $api_key = "qCaJfO73WWJfh9FHBXYd"; 
    $message = $text; 
-   $to = "94768612036";
+   $to = $contact;
    $sender_id = "NotifyDEMO"; 
-   
+    
    try {
       $api_instance->sendSMS($user_id, $api_key, $message, $to, $sender_id);
-      $sql = "INSERT INTO otprequest otp,entryId,date VALUES(?,?,?)";
+      $_SESSION['otp']=$otp;
+      header("Location:/fadts/village/nicSearchModel?view=fundRelease&entryId=$entryId&nic=$nic"); 
+      exit();
+      //require 'connection.php';
+
+      // $sql = "SELECT * FROM otprequest WHERE entryId=?";
+      // $stmt = mysqli_stmt_init($con);
+
+      // if(!mysqli_stmt_prepare($stmt,$sql)){
+      //    mysqli_close($con);
+      //    header("Location:/fadts/village/$view?searcherror=db_conn_err");
+      //    exit();
+      // }else{
+      //    mysqli_stmt_bind_param($stmt,"s",$entryId);
+      //    mysqli_stmt_execute($stmt);
+      //    header("Location:/fadts/village/fundRelease?entryId=$entryId&time=$sendedTime"); 
+      //    exit();
+      // }
+
+
+      // $sql = "INSERT INTO otprequest (otp,entryId) VALUES(?,?)";
+      // $stmt = mysqli_stmt_init($con);
+
+      // if(!mysqli_stmt_prepare($stmt,$sql)){
+      //    mysqli_close($con);
+      //    header("Location:/fadts/village/$view?searcherror=db_conn_err");
+      //    exit();
+      // }else{
+      //    mysqli_stmt_bind_param($stmt,"si",$otpHashed,$entryId);
+      //    mysqli_stmt_execute($stmt);
+      //    header("Location:/fadts/village/fundRelease?entryId=$entryId&time=$sendedTime"); 
+      //    exit();
+      // }
+      
+   }catch(Exception $e){
+      // echo 'Exception when calling SmsApi->sendSMS: ', $e->getMessage(), PHP_EOL;
+      header("Location:/fadts/village/fundRelease?otp=otp_resend&entryId=$entryId"); 
+      exit();
+   }
+
+    
+
+}elseif(isset($_POST['confirm']) && isset($_POST['otp']) && isset($_GET['entryId'])){
+
+   $otp = $_POST['otp'];
+   $entryId = $_GET['entryId'];
+      
+
+   if($otp==$_SESSION['otp']){
+      require 'connection.php';
+
+      $sql = "UPDATE recipient SET deliveryStatus = 1,deliveryTime = NOW() WHERE entryId = $entryId";
       $stmt = mysqli_stmt_init($con);
 
       if(!mysqli_stmt_prepare($stmt,$sql)){
          mysqli_close($con);
-         header("Location:/fadts/village/$view?searcherror=db_conn_err");
-         exit();
+         header("Location:/fadts/divisional/fundRelease?error=db_conn_err");
+         exit();     
       }else{
-         mysqli_stmt_bind_param($stmt,"sss",$otp,$entryId,$sendedTime);
-         mysqli_stmt_execute($stmt);
-         $result = mysqli_stmt_get_result($stmt);
+
+         if(mysqli_stmt_execute($stmt)) {
+            mysqli_close($con);
+            header("Location:/fadts/divisional/fundRelease?searcherror=release_success");
+            exit();
+         }else{
+            mysqli_close($con);
+            header("Location:/fadts/divisional/fundRelease?searcherror=release_error");
+            exit();
+         }
       }
-      
-   } catch (Exception $e) {
-      // echo 'Exception when calling SmsApi->sendSMS: ', $e->getMessage(), PHP_EOL;
-      header("Location:/fadts/village/fundRelease"); ////////////?????
+   }else{
+      header("Location:/fadts/village/nicSearchModel?view=fundRelease&entryId=$entryId&nic=$nic"); 
       exit();
    }
-
-   
-
-}elseif(isset($_POST['confirm']) && isset($_GET['entryId'])){
 
 }else{
    header("Location:/fadts/village/fundRelease");
